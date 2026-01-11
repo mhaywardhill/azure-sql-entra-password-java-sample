295ffb78903e1a79d1b973082c173e3547d60f2c
mhaywardhill <mhaywardhill@microsoft.com>
2026-01-11 14:19:50 +0000

    Make run.sh executable

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Ensure required env vars are present
 : "${AZURE_SQL_SERVER:?set AZURE_SQL_SERVER}"
 : "${AZURE_SQL_DATABASE:?set AZURE_SQL_DATABASE}"
-: "${AZURE_SQL_USER:?set AZURE_SQL_USER"
+: "${AZURE_SQL_USER:?set AZURE_SQL_USER}"
 : "${AZURE_SQL_PASSWORD:?set AZURE_SQL_PASSWORD}"
 
 mvn -q -DskipTests package
